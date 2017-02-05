f76dcced8efc1fc030b1c124d09213a9ffc56c90
Jose Maria <melopsitaco@gmail.com>
2017-02-05 16:24:00 +0100

    Face detector with keypoints from FKEFaceDetector

--- src/main/java/com/calabozo/facedetector/App.java
+++ src/main/java/com/calabozo/facedetector/App.java
@@ -6,9 +6,13 @@ import org.openimaj.image.FImage;
 import org.openimaj.image.MBFImage;
 import org.openimaj.image.colour.RGBColour;
 import org.openimaj.image.colour.Transforms;
-import org.openimaj.image.processing.face.detection.DetectedFace;
 import org.openimaj.image.processing.face.detection.FaceDetector;
-import org.openimaj.image.processing.face.detection.HaarCascadeDetector;
+import org.openimaj.image.processing.face.detection.keypoints.FKEFaceDetector;
+import org.openimaj.image.processing.face.detection.keypoints.FacialKeypoint;
+import org.openimaj.image.processing.face.detection.keypoints.KEDetectedFace;
+import org.openimaj.math.geometry.point.Point2d;
+import org.openimaj.math.geometry.point.Point2dImpl;
+import org.openimaj.math.geometry.shape.Shape;
 import org.openimaj.video.Video;
 import org.openimaj.video.VideoDisplay;
 import org.openimaj.video.VideoDisplayListener;
@@ -31,13 +35,24 @@ public class App {
 		VideoDisplay<MBFImage> display = VideoDisplay.createVideoDisplay(video);
 
 		display.addVideoListener(new VideoDisplayListener<MBFImage>() {
-			FaceDetector<DetectedFace, FImage> fd = new HaarCascadeDetector(100);
+			FaceDetector<KEDetectedFace, FImage> fd = new FKEFaceDetector();
+
 			public void beforeUpdate(MBFImage frame) {
-				List<DetectedFace> faces = fd.detectFaces(Transforms.calculateIntensity(frame));
-				for (DetectedFace face : faces)
-					frame.drawShape(face.getBounds(), RGBColour.RED);
+				List<KEDetectedFace> faces = fd.detectFaces(Transforms.calculateIntensity(frame));
+				for (KEDetectedFace face : faces) {
+					Shape shape = face.getShape();
+					frame.drawShape(shape, RGBColour.RED);
+					Point2d faceOrigin = new Point2dImpl(shape.minX(), shape.minY());
+					FacialKeypoint[] keypoints = face.getKeypoints();
+					for (FacialKeypoint point : keypoints) {
+						point.position.translate(faceOrigin);
+						frame.drawPoint(point.position, RGBColour.WHITE, 10);
+					}
+				}
 			}
-			public void afterUpdate(VideoDisplay<MBFImage> display) {}
+
+			public void afterUpdate(VideoDisplay<MBFImage> display) {
+			}
 		});
 	}
 }
